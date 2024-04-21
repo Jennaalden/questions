5f44096cf11f25e23b4cd5f34818413f9de8f801
Jenna Alden <aldenjenna04@gmail.com>
2024-04-21 18:47:21 -0400

    update

--- src/question-slide.js
+++ src/question-slide.js
@@ -1,54 +1,60 @@
 import { LitElement, html, css } from 'lit';
+import { DDD } from "@lrnwebcomponents/d-d-d/d-d-d.js";
 
+export class QuestionSlide extends DDD {
 
-export class questionSlide extends LitElement {
-
-
-static get tag() {
-    return 'question-slide';
-}
-
-constructor(){
-    super();
-    this.prompt = [];
-}
-
-
-static get styles() {
-    return css`
-    :host {
- display: block;
+    static get tag() {
+        return 'question-slide';
     }
 
-    .question-ui {
-        width:100%;
-        height: 100%;
-        position: relative;
-        top: 10px;
-        left: 0px;
-        z-index: 9999;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        pointer-events: all;
+    constructor() {
+        super();
+        this.blocks = [];
+        this.correctInput = ["Correct"];
     }
 
-    .question-modal {
-        background-color: white;
-        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-        text-align: center;
-        width: 80vw;
-        border: 2px solid white;
-        box-sizing: border-box;
-        padding: 20px;
-        border: 5px dotted #ffffff;
+    
+
+    static get styles() {
+        return css`
+            :host {
+                display: block;
+            }
+            .answerbox {
+                display: inline-block;
+                height: 100px;
+                width: 400px;
+                margin: 10px;
+                border: solid 3px grey;
+            }
+            .hovered {
+                background: grey;
+                border-style: dashed; 
+            }
+    `;
     }
+    render() {
+        return html`
+        <div class="quiz-container">
+            <div class="image-container">
+                <img src="https://hax.psu.edu/7d3549e0.png">
+            </div>
+        </div>
 
+        <div class="quiz-options">
+            <div class="answer-option" draggable="true">wrong answer</div>
+            <div class="answer-option" draggable="true">right answer</div>
+            <div class="answer-option" draggable="true">another wrong answer</div>
+        </div>
 
-    
-    
-    
-`}
+        <div class="answerbox" @drop="${this.handleDrop}" @dragover="${this.handleDragOver}">
+        <p>correct answer here!</p>
+        </div>
+        `;
+    }
+    static get properties() {
+        return {
+            blocks: {type: array}
+        };
+    }
 }
-
-globalThis.customElements.define(questionSlide.tag, questionSlide);
